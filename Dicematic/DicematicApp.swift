19efa396f0e757c9c2d80bd69cd9f54c521715b9
s3rg4sh <gashysh@protonmail.com>
2022-07-28 13:44:30 +0300

    Initial Commit

---
+++ Dicematic/DicematicApp.swift
@@ -0,0 +1,17 @@
+//
+//  DicematicApp.swift
+//  Dicematic
+//
+//  Created by Сергій Гашенко on 28.07.2022.
+//
+
+import SwiftUI
+
+@main
+struct DicematicApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
